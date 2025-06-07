29147392f3a0f52682ea99d0b768caa6169f08d4
Pratik Ghadge <pratikhg.2001@gmail.com>
2025-06-07 13:59:43 +0530

    [iOS][PracticeProject] Initialized Project

---
+++ HelloCoffeeApp/HelloCoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeAppApp.swift
+//  HelloCoffeeApp
+//
+//  Created by Pratik Ghadge on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
